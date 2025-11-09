efde22c7ba09281602925c4e29fec5a24b12f9d3
Alex Buckley <alexmeb81@outlook.com>
2025-11-09 01:59:09 -0800

    Implemented function to print table of process execution with different scheudling algorithms

--- include/schedule_display.h
+++ include/schedule_display.h
@@ -9,11 +9,19 @@
 #define ASC_BOLD "\x1b[1m"
 
 #define ASC_RED_FG "\x1b[31m"
-#define ASC_BLUE_FG "\x1b[34m"
 #define ASC_GREEN_FG "\x1b[32m"
+#define ASC_YELLOW_FG   "\x1b[33m"
+#define ASC_BLUE_FG "\x1b[34m"
 #define ASC_MAGENTA_FG  "\x1b[35m"
 #define ASC_CYAN_FG     "\x1b[36m"
-#define ASC_YELLOW_FG   "\x1b[33m"
+
+
+#define ASC_RED_BG      "\x1b[41m"
+#define ASC_GREEN_BG    "\x1b[42m"
+#define ASC_YELLOW_BG   "\x1b[43m"
+#define ASC_BLUE_BG     "\x1b[44m"
+#define ASC_MAGENTA_BG  "\x1b[45m"
+#define ASC_CYAN_BG     "\x1b[46m"
 
 #define LINE_START ASC_BLUE_FG "|" ASC_RESET
 
--- src/schedule_display.c
+++ src/schedule_display.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 #include "../include/fmt.h"
 #include "../include/schedule_display.h"
 
 
 #define PROC_TABLE_DIV "+------------+--------------+------------+----------------+--------------+-----------------+"
+#define PROC_COLORS 6
+
+#define BOOL char
+#define TRUE 1
+#define FALSE 0
 
 
 void processPrintHeader(const char* linePrefix) {
@@ -43,3 +50,52 @@ void execProcessPrint(const Process* p) {
     printf(LINE_START "\n" LINE_START "\n" LINE_START);
 }
 
+
+void execProcessTable(Process*** procs, int* completionTimes, int numProcs) {
+    BOOL* isFinished = calloc(numProcs, sizeof(BOOL));
+    size_t numFinished = 0;
+    int time = 0;
+    
+    printf("\n");
+    printf("+--------------------------------+\n");
+
+    while(numFinished < numProcs) {
+        printf("|%d|", time + 1);
+
+        for(int i = 0; i < numProcs; i++) {
+            char *color = ASC_RESET;
+            int process = INT_MIN;
+
+            if(time < completionTimes[i]) { // 
+                if(procs[i][time]) { // Assign color  and value to process
+                    process = procs[i][time]->processId; // Get running process ID
+
+                    switch (process % PROC_COLORS) { // Assign cell color
+                        case 1: color = ASC_RED_BG; break;
+                        case 2: color = ASC_GREEN_BG; break;
+                        case 3: color = ASC_YELLOW_BG; break;
+                        case 4: color = ASC_BLUE_BG; break;
+                        case 5: color = ASC_MAGENTA_BG; break;
+                        case 6: color = ASC_CYAN_BG; break;
+                        default: color = ASC_RESET; break;
+                    }
+                }
+            } else if(time == completionTimes[i]) { // Process finished
+                numFinished++;
+            }
+            
+            if(process != INT_MIN) // Display running process
+                printf("%s%s %d %s|", ASC_BOLD, color, process, ASC_RESET);
+            else // Display empty cell
+                printf("%s%s   %s|", ASC_BOLD, color, ASC_RESET);
+
+            
+        }
+
+        // Advance time unit
+        printf("\n+--------------------------------+\n");
+        time++;
+    }
+
+    printf("\n");
+}
